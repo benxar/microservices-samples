4710c48057d7dfc7ce1b2bcea90a6e50627f9273
Cristian Sostaric <csostari@redhat.com>
2020-06-24 15:10:21 -0300

    fix port

--- mock-service/server.js
+++ mock-service/server.js
@@ -32,7 +32,7 @@ app.use( keycloak.middleware({
   admin: '/'
 }));
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4200,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 // Allow CORS
